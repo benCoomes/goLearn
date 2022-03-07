0c81679f964fe2d4348a03ffe798cf33360712c6
Ben Coomes <bcoomes96@gmail.com>
2022-03-07 06:55:19 -0500

    Add hellos function and use it

--- 001_modulesAndTests/greetings/greetings.go
+++ 001_modulesAndTests/greetings/greetings.go
@@ -16,6 +16,21 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 // Go executes init functions automatically at program startup,
 // after global variables have been initialized.
 func init() {
